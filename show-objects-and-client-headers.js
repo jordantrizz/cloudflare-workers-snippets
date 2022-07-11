4c26ee0da8c7e73a6ce8fc9e362173ac8b23dfe7
Jordan <jtrask@gmail.com>
2022-07-11 14:14:19 -0400

    Update show-objects-and-client-headers.js

--- show-objects-and-client-headers.js
+++ show-objects-and-client-headers.js
@@ -1,49 +1,91 @@
-function printArray(arr) {
-    if ( typeof(arr) == "object") {
-        for (var i = 0; i < arr.length; i++) {
-            printArray(arr[i]);
-        }
-    }
-    else console.log(arr);
-}
+// Install
+//   1. Create a worker and deploy the following code.
+//   2. Setup a route for domian.com/* and *.domain.com/*
+//   3. Create a transform rule.
+//          -HTTP Request Header Modification
+//          -(ip.src eq 76.70.117.218)
+//          -Set header "fail2banblock" = "1"
+//
 
-addEventListener('fetch', event => {
-  const d =
-    event.request.cf !== undefined
-      ? event.request.cf
-      : { error: 'The `cf` object is not available inside the preview.' };
-
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request))
+})
  
-  e = event.request;
+async function handleRequest(request) {
+
+  let modifiedHeaders = new Headers()
+  modifiedHeaders.set('Content-Type', 'text/html')
+  modifiedHeaders.append('Pragma', 'no-cache')
+  modifiedHeaders.append('fail2bancfblock', 'enabled')
+ 
+  const clientIP = request.headers.get('CF-Connecting-IP');
+  const request_headers = request.headers.get('fail2banblock') || '';
+
+  e = request;
   let client_headers = '';
   for (const element of e.headers) {
       client_headers = client_headers.concat(element[0], " => ", element[1], "<br>")
   }
-   
-  let cf_objects = '';
-  //cf_objects = JSON.stringify(d, null, 2);
-  for (var item in d) {
-    if ( typeof(d[item]) == "object") {
-      cf_objects += item + ': Array-><br>';
-        for (var subitem in d[item]) {
-          cf_objects += '    ' + subitem + ': ' + d[item][subitem]+';<br>';
-        }
-    } else {
-    cf_objects += item + ': ' + d[item]+';<br>';
-    }
-  }
-cf_objects += '<h2>JSON4</h2><code>' + JSON.stringify(d,'<br>') + '</code>';
 
- 
-  var host = e.headers.get("host")
-  //let d = "test"
-  let html = '<h1>CF Objects</h1><pre>' + cf_objects + '</pre><h1>Client headers</h1><pre>' + client_headers +'</pre>';
-  
-  return event.respondWith(
-    new Response(JSON.stringify(html, null, 2), {
-      headers: {
-        'content-type': 'text/html;charset=UTF-8',
-      },
-    })
-  );
-});
+let block_page = `
+<!doctype html>
+<head>
+<title>Security Blocked </title>
+<style>
+  body { 
+        text-align: center; 
+        padding: 150px; 
+        background: white; 
+        background-size: cover;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+      }
+    .content {
+        background-color: rgba(255, 255, 255, 0.75); 
+        background-size: 100%;      
+        color: inherit;
+        padding: 1px 100px 10px 100px;
+        border-radius: 15px;        
+    }
+  h1 { font-size: 40pt;}
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 75%; margin: 0 auto; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+<meta http-equiv="refresh" content="5; URL=/" />
+</head>
+<body>
+<article> 
+  <div class="background">
+    <div class="content">
+      <h1>You've been blocked due to a security rule.</h1>
+      <p>Your IP ${clientIP} has been blocked temporarily</p>
+        <p><b>-Management</b></p>
+    </div>
+  </div>
+  <div>
+    <div style="font-weight:bold;">debug.</div>
+    <div style="font-weight:bold;">request_headers</div>
+    <code>${request_headers}</code>
+    <br>
+    <div style="font-weight:bold;">client_headers</div>
+    <code>${client_headers}</code>
+  </div>
+</article>
+</body>
+`;
+
+  // Return block page if you're not calling from a trusted IP
+  if (clientIP !== "127.0.0.1")  {
+    return new Response(block_page, { headers: modifiedHeaders})
+  // Return a block page if Fail2banBlock = 1
+  } else if (request_headers.includes('1')) {
+    return new Response(block_page, { headers: modifiedHeaders})
+  } else {
+    //Allow users that aren't block    
+    const passthrough = new Response(response.body, response);
+    passthrough.headers.append('fail2bancfblock', 'enabled');
+    return passthrough;
+  }
+}
